6e382c6ce3227564cb6804ab2512ef3dad9aa270
ThisNicknameIsTaken <32493975+ThisNicknameIsTaken@users.noreply.github.com>
2019-03-19 09:35:05 +0200

    Update Figure.c

--- dk81/Romakh Vitalii/hw2_var5/Figure.c
+++ dk81/Romakh Vitalii/hw2_var5/Figure.c
@@ -1,17 +1,21 @@
-#include "Figure.h"
 #include <stdio.h>
+#include "Figure.h"
 
-
-Dot *createDot() {
+Dot *createDot(int xCoor, int yCoor) { //создание точки вручную через консоль
 	Dot *d = (Dot *)malloc(sizeof(Dot));
-	
-	int x, y;
 
-	printf("Input x:\n");
-	scanf_s("%i",&x);
+	int x = xCoor;
+	int y = yCoor;
 
-	printf("Input y:\n");
-	scanf_s("%i", &y);
+	if (x == ERROR) {
+		printf("Input x:\n");
+		scanf_s("%i", &x);
+	}
+
+	if (y == ERROR) {
+		printf("Input y:\n");
+		scanf_s("%i", &y);
+	}
 
 	d->x = x;
 	d->y = y;
@@ -19,7 +23,7 @@ Dot *createDot() {
 	return d;
 }
 
-Dot *generateDot(int x, int y, char id) {
+Dot *generateDot(int x, int y, char id) { //Конструктор точки
 	Dot *d = (Dot *)malloc(sizeof(Dot));
 
 	d->x = x;
@@ -29,74 +33,70 @@ Dot *generateDot(int x, int y, char id) {
 	return d;
 }
 
-Trapezium *generateTrapezium(int radius) {
+Trapezium *generateTrapezium(int radius) {  //Генерация трапеции, разброс точок с расстоянием до радиуса
 	time_t t;
 	srand((unsigned)time(&t));
 	Trapezium *trap = (Trapezium *)malloc(sizeof(Trapezium));
-	
+
 	trap->leftBottomCorner = generateDot(0, 0, 'A');
 	trap->leftTopCorner = generateDot(rand() % radius + 1, rand() % radius + 1, 'B');
-	trap->rightTopCorner = generateDot(trap->leftTopCorner->x + rand() % radius + 1, rand() % radius + 1, 'C');
-	trap->rightBottomCorner = generateDot(trap->rightTopCorner->x + rand() % radius + 1, trap->rightTopCorner->y - rand() % radius - 1,'D');
+	trap->rightTopCorner = generateDot(trap->leftTopCorner->x + rand() % radius + 1, trap->leftTopCorner->y , 'C');
+	trap->rightBottomCorner = generateDot(trap->rightTopCorner->x + rand() % radius + 1, trap->leftBottomCorner->y, 'D');
 
 	trap->square = calculateTrapeziumSquare(trap);
 
 	return trap;
 }
 
-Trapezium *createTrapezium() {
+Trapezium *createTrapezium() { // создание Трапеции вручную через консоль
 	Trapezium *trap = (Trapezium *)malloc(sizeof(Trapezium));
-	
-	
-			printf("Input left bottom dot - A:\n");
-			trap->leftBottomCorner = createDot();
-			trap->leftBottomCorner->id = 'A';
-
-			printf("\nInput left top dot - B:\n");
-			trap->leftTopCorner = createDot();
-			trap->leftTopCorner->id = 'B';
-
-			if (trap->leftTopCorner->y <= trap->leftBottomCorner->y) {
-				printf("Top corner couldn't be lower than bottom corner!\n");
-				return ERROR;
-			}
-
-			printf("\nInput right top dot - C:\n");
-			trap->rightTopCorner = createDot();
-			trap->rightTopCorner->id = 'C';
-
-			if (trap->rightTopCorner->x <= trap->leftTopCorner->x) {
-				printf("Left corner couldn't be righter than left corner!\n");
-				return ERROR;
-			}
-			
 
 
-			printf("\nInput right bottom dot - D:\n");
-			trap->rightBottomCorner = createDot();
-			trap->rightBottomCorner->id = 'D';
+	printf("Input left bottom dot - A:\n");
+	trap->leftBottomCorner = createDot(ERROR, ERROR);
+	trap->leftBottomCorner->id = 'A';
 
-			if (trap->rightBottomCorner->y >= trap->rightTopCorner->y) {
-				printf("Top corner couldn't be lower than bottom corner!\n");
-				return ERROR;
-			}
-			else if (trap->rightBottomCorner->x <= trap->leftBottomCorner->x) {
-				printf("Left corner couldn't be righter than left corner!\n");
-				return ERROR;
-			}
+	printf("\nInput left top dot - B:\n");
+	trap->leftTopCorner = createDot(ERROR, ERROR);
+	trap->leftTopCorner->id = 'B';
 
-		trap->square = calculateTrapeziumSquare(trap);
-		return trap;
+	if (trap->leftTopCorner->y <= trap->leftBottomCorner->y) {
+		printf("Top corner couldn't be lower than bottom corner!\n");
+		return ERROR;
+	}
+
+	printf("\nInput right top dot x - C:\n");
+	trap->rightTopCorner = createDot(ERROR,trap->leftTopCorner->y);
+	trap->rightTopCorner->id = 'C';
+
+	if (trap->rightTopCorner->x <= trap->leftTopCorner->x) {
+		printf("Left corner couldn't be righter than left corner!\n");
+		return ERROR;
+	}
+
+
+
+	printf("\nInput right bottom dot x - D:\n");
+	trap->rightBottomCorner = createDot(ERROR, trap->leftBottomCorner->y);
+	trap->rightBottomCorner->id = 'D';
+
+    if (trap->rightBottomCorner->x <= trap->leftBottomCorner->x) {
+		printf("Left corner couldn't be righter than left corner!\n");
+		return ERROR;
+	}
+
+	trap->square = calculateTrapeziumSquare(trap);
+	return trap;
 }
 
-void showDot(Dot *d) {
-	printf("x = %i\n",d->x);
+void showDot(Dot *d) { //Вывести точку на экран
+	printf("x = %i\n", d->x);
 	printf("y = %i\n", d->y);
 }
 
-void showTrapezium(Trapezium *t) {
+void showTrapezium(Trapezium *t) { //Вывести данные про фигуру на экран
 
-	printTrapezium(t);
+	printTrapezium(t); // Вывести фигуру на экран
 
 	printf("\n---------------------------------------------\n");
 
@@ -111,8 +111,8 @@ void showTrapezium(Trapezium *t) {
 
 	printf("Right Bottom Dot - D:\n");
 	showDot(t->rightBottomCorner);
-	
-		
+
+
 	printf("Length Line AB: %f\n", calculateLineLength(t->leftBottomCorner, t->leftTopCorner));
 	printf("Length Line BC: %f\n", calculateLineLength(t->leftTopCorner, t->rightTopCorner));
 	printf("Length Line CD: %f\n", calculateLineLength(t->rightTopCorner, t->rightBottomCorner));
@@ -123,11 +123,11 @@ void showTrapezium(Trapezium *t) {
 	printf("\n");
 }
 
-void deleteDot(Dot *dot) {
+void deleteDot(Dot *dot) { // удаление точки
 	free(dot);
 }
 
-void deleteTrapezium(Trapezium *trapezium) {
+void deleteTrapezium(Trapezium *trapezium) {// удаление трапеции
 	deleteDot(trapezium->leftBottomCorner);
 	deleteDot(trapezium->leftTopCorner);
 	deleteDot(trapezium->rightTopCorner);
@@ -136,16 +136,18 @@ void deleteTrapezium(Trapezium *trapezium) {
 	free(trapezium);
 }
 
-float calculateLineLength(Dot *a, Dot *b) {
+float calculateLineLength(Dot *a, Dot *b) {// расчёт длины отрезка между двумя точками
 	int deltaX = b->x - a->x;
 	int deltaY = b->y - a->y;
 
-	float result = sqrt(deltaX*deltaX + deltaY* deltaY);
+	float result = sqrt(deltaX*deltaX + deltaY * deltaY);
 	return result;
 }
 
 
- void printTrapezium(Trapezium *trap) {
+void printTrapezium(Trapezium *trap) { // Вывод на экран
+
+	// Я ищу крайные точки для того чтобы найти высоту и длину для цикла для того чтобы напечатать ттрапецию
 	Dot* leftDot = findLeftDot(trap);
 	Dot* rightDot = findRightDot(trap);
 	Dot* topDot = findTopDot(trap);
@@ -161,11 +163,11 @@ float calculateLineLength(Dot *a, Dot *b) {
 
 	int resDotID = ERROR;
 
-	for (int i = height+1; i >= -1; i--)
+	for (int i = height + 1; i >= -1; i--)
 	{
-		for (int j = -1; j <= width+1; j++)
+		for (int j = -1; j <= width + 1; j++)
 		{
-			resDotID = compareDot(trap->leftBottomCorner, j, i, offsetX, offsetY, resDotID);
+			resDotID = compareDot(trap->leftBottomCorner, j, i, offsetX, offsetY, resDotID);  // Для того чтобы понять какая точка находится в координатах.
 			resDotID = compareDot(trap->leftTopCorner, j, i, offsetX, offsetY, resDotID);
 			resDotID = compareDot(trap->rightTopCorner, j, i, offsetX, offsetY, resDotID);
 			resDotID = compareDot(trap->rightBottomCorner, j, i, offsetX, offsetY, resDotID);
@@ -174,9 +176,8 @@ float calculateLineLength(Dot *a, Dot *b) {
 				printf("%c", resDotID);
 				resDotID = ERROR;
 				continue;
-			}else if (isOnLine(trap->leftBottomCorner, trap->leftTopCorner,j,i) == 1 || isOnLine(trap->leftTopCorner, trap->rightTopCorner, j, i) == 1 || isOnLine(trap->rightTopCorner, trap->rightBottomCorner, j, i) == 1 || isOnLine(trap->rightBottomCorner, trap->leftBottomCorner, j, i) == 1) {
-				printf("*");
-			}else{
+			}
+			else {
 				printf(" ");
 			}
 
@@ -185,7 +186,7 @@ float calculateLineLength(Dot *a, Dot *b) {
 	}
 }
 
-Dot* findLeftDot(Trapezium *trap) {
+Dot* findLeftDot(Trapezium *trap) { // Поиск крайней левой точки
 	if (trap->leftBottomCorner->x < trap->leftTopCorner->x) {
 		return trap->leftBottomCorner;
 	}
@@ -197,7 +198,7 @@ Dot* findLeftDot(Trapezium *trap) {
 	}
 }
 
-Dot* findRightDot(Trapezium *trap) {
+Dot* findRightDot(Trapezium *trap) { // Поиск крайней правой точки
 	if (trap->rightBottomCorner->x < trap->rightTopCorner->x) {
 		return trap->rightTopCorner;
 	}
@@ -209,7 +210,7 @@ Dot* findRightDot(Trapezium *trap) {
 	}
 }
 
-Dot* findTopDot(Trapezium *trap) {
+Dot* findTopDot(Trapezium *trap) {// Поиск крайней верхней точки
 	if (trap->leftTopCorner->y < trap->rightTopCorner->y) {
 		return trap->rightTopCorner;
 	}
@@ -221,7 +222,7 @@ Dot* findTopDot(Trapezium *trap) {
 	}
 }
 
-Dot* findBottomDot(Trapezium *trap) {
+Dot* findBottomDot(Trapezium *trap) { // Поиск крайней нижней точки
 	if (trap->leftBottomCorner->y < trap->rightBottomCorner->y) {
 		return trap->leftBottomCorner;
 	}
@@ -233,7 +234,7 @@ Dot* findBottomDot(Trapezium *trap) {
 	}
 }
 
-int compareDot(Dot *d, int x, int y,int offsetX, int offsetY, int currentId){
+int compareDot(Dot *d, int x, int y, int offsetX, int offsetY, int currentId) { //Функия создана для того чтобы определить одинакова ли точка d и точка с координатами x и y, offsetX и offsetY даны для того чтобы перевести точку с х и у в систему координат точки d
 	if (currentId != ERROR) {
 		return currentId;
 	}
@@ -245,46 +246,9 @@ int compareDot(Dot *d, int x, int y,int offsetX, int offsetY, int currentId){
 	}
 }
 
-int isOnLine(Dot *a, Dot *b, int x, int y) {
-Dot *c = generateDot(x,y,'N');
-	 
-    float abSide = calculateLineLength(a, b); //a
-	float bcSide= calculateLineLength(b, c); // c
-	float caSide = calculateLineLength(c, a); //b
-
-	if (abSide >= bcSide + caSide || bcSide >= abSide + caSide || caSide >= bcSide + abSide) { // сумма длин каждых двух сторон должна быть больше длины третьей стороны.
-		return 0;
-	}
-
-	float angleBeta = 0.5;
-	float multy = 2 * abSide*caSide;
-	if (multy != 0) {
-		angleBeta = (abSide*abSide + bcSide * bcSide - caSide * caSide) / multy;
-	}
-	
-
-	int dx1 = b->x - a->x;
-	int dy1 = b->y - a->y;
-
-	int dx = x - a->x;
-	int dy = y - a->y;
-
-	float s = dx1 * dy - dx * dy1;
-
-	float ab = sqrt(dx1 * dx1 + dy1 * dy1);
-
-	float h = s / ab;
 
 
-
-	if ((h <= 1 && h >= 0) && (angleBeta > 0 || angleBeta < 0.7))
-	{
-		return 1;
-	}
-	return 0;
-}
-
-float calculateTrapeziumSquare(Trapezium *trap) {
+float calculateTrapeziumSquare(Trapezium *trap) {//Расчет площади трапеции
 	float a = trap->leftBottomCorner->x * trap->leftTopCorner->y + trap->leftTopCorner->x * trap->rightTopCorner->y + trap->rightTopCorner->x * trap->rightBottomCorner->y + trap->rightBottomCorner->x * trap->leftBottomCorner->y;
 	float b = trap->leftBottomCorner->y * trap->leftTopCorner->x + trap->leftTopCorner->y * trap->rightTopCorner->x + trap->rightTopCorner->y * trap->rightBottomCorner->x + trap->rightBottomCorner->y * trap->leftBottomCorner->x;
 
